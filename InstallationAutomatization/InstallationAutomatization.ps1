8382dc3874469ca71b3bd11e46b314f683ac8f3b
Timo Gremler <timo@timogremler.de>
2020-05-13 16:39:20 +0200

    Automatisierte Installation von Software

---
+++ InstallationAutomatization/InstallationAutomatization.ps1
@@ -0,0 +1,48 @@
+Set-ExecutionPolicy AllSigned
+
+#Installed Choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#VS Code
+choco install vscode -y
+#Reload Path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+#Code Extensions
+code --install-extension ms-dotnettools.csharp
+code --install-extension donjayamanne.githistory
+code --install-extension james-yu.latex-workshop
+code --install-extension ritwickdey.liveserver
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension ms-vscode.powershell
+code --install-extension ms-python.python
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ban.spellright
+
+
+choco install visualstudio2019community -y
+
+choco install notepadplusplus -y
+
+choco install firefox -y
+
+choco install thunderbird -y
+
+choco install steam -y
+
+choco install keepass -y
+
+choco install git -y
+
+choco install discord -y 
+
+choco install vmwareworkstation -y
+
+choco install boxcryptor -y
+ 
+#Create Shortcut to Desktop
+choco install cmder -y
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Cmder.lnk")
+$Shortcut.TargetPath = "C:\tools\Cmder\Cmder.exe"
+$Shortcut.Save()
