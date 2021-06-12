b9b8ad49ab6f157d3ad9004cb274ad63eb44462c
Timo Gremler <timo@timogremler.de>
2021-06-12 20:08:02 +0200

    Laufwerkmapping hinzugefügt

--- InstallationAutomatization/InstallationAutomatization.ps1
+++ InstallationAutomatization/InstallationAutomatization.ps1
@@ -6,15 +6,14 @@ winget install vscode
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 #Code Extensions
-code --install-extension james-yu.latex-workshop
-code --install-extension ritwickdey.liveserver
+code --install-extension james-yu.latex-workshop   
+code --install-extension ritwickdey.liveserver 
 code --install-extension ms-vscode.powershell
 code --install-extension ms-python.python
 code --install-extension ban.spellright
 code --install-extension alireza94.theme-gotham
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension coenraads.bracket-pair-colorizer-2
-
 
 winget install Microsoft.VisualStudio.2019.Community 
 
@@ -32,9 +31,7 @@ winget install git
 
 winget install discord 
 
-winget install vmwareworkstation 
-
-#winget install boxcryptor 
+winget install vmwareworkstation  
 
 #winget install miktex 
 
@@ -46,10 +43,26 @@ winget install gitkraken
 
 #winget install hackfont 
 
-#winget install microsoft-windows-terminal 
+winget install Microsoft.WindowsTerminal 
+
+winget install ubuntu
 
 #winget install autohotkey 
 
 #Setting up path enviroment for my scripts
 #[Environment]::SetEnvironmentVariable("Path", $env:Path + ";E:\2_Computer\Projekte\ScriptCollection\Batch", "User")
 #[Environment]::SetEnvironmentVariable("Path", $env:Path + ";E:\2_Computer\Projekte\ProjectInitializationAutomation\windows_OS", "User")
+
+#-------------Laufwerke-------------
+
+$cred = Get-Credential 
+
+New-PSDrive -Name "Timo" -PSProvider "FileSystem" -Root "\\192.168.178.13\Timo" -Credential $cred
+New-PSDrive -Name "Plex" -PSProvider "FileSystem" -Root "\\192.168.178.13\Plex" -Credential $cred
+New-PSDrive -Name "Archiv" -PSProvider "FileSystem" -Root "\\192.168.178.13\Archiv" -Credential $cred
+
+Net Use
+
+
+
+
